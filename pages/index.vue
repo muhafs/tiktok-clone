3304ede072786261379ce68dcd6f434afd2db843
muhamadhaspin <muhamadhaspin@gmail.com>
2023-07-25 19:03:20 +0700

    initial project with dependencies

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="bg-red-500 text-white text-3xl text-center py-6 font-bold">
+		Test
+
+		<Icon name="🚀" size="100" />
+	</div>
+</template>
