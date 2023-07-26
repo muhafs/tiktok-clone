3ac1de756ac48cded4f52a285a851c0658f5dedb
muhamadhaspin <muhamadhaspin@gmail.com>
2023-07-26 15:36:13 +0700

    --ft: create UploadLayout component

---
+++ layouts/UploadLayout.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <div class="h-[100vh] bg-[#F8F8F8]">
+        <TopNav />
+
+        <div class="mx-auto flex w-full max-w-[1140px] justify-between px-2">
+            <slot />
+        </div>
+    </div>
+</template>
