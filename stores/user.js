fbd7ebd7f6758064b98f8c22287abf96cc1d8b33
muhamadhaspin <muhamadhaspin@gmail.com>
2023-07-31 21:43:04 +0700

    --ft : setup auth api methods

--- stores/user.js
+++ stores/user.js
@@ -1,4 +1,7 @@
 import { defineStore } from 'pinia'
+import axios from '~/plugins/axios'
+
+const $axios = axios().provide.axios
 
 export const useUserStore = defineStore('user', {
     state: () => ({
@@ -7,11 +10,45 @@ export const useUserStore = defineStore('user', {
         bio: '',
         image: '',
     }),
-    getters: {
-        //
-    },
     actions: {
-        //
+        async getTokens() {
+            await $axios.get('/sanctum/csrf-cookie')
+        },
+        async login(email, password) {
+            await $axios.post('/login', {
+                email,
+                password,
+            })
+        },
+        async register(name, email, password, confirmPassword) {
+            await $axios.post('/register', {
+                name,
+                email,
+                password,
+                password_confirmation: confirmPassword,
+            })
+        },
+        async getUser() {
+            let res = await $axios.get('/api/logged-in-user')
+            let data = res.data[0]
+
+            this.$state.id = data.id
+            this.$state.name = data.name
+            this.$state.bio = data.bio
+            this.$state.image = data.image
+        },
+        async logout() {
+            await $axios.post('/logout')
+
+            this.resetUser()
+        },
+        resetUser() {
+            this.$state.id = ''
+            this.$state.name = ''
+            // this.$state.email = ''
+            this.$state.bio = ''
+            this.$state.image = ''
+        },
     },
     persist: true,
 })
